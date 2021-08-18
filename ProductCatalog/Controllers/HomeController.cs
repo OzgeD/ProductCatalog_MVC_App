244aef807d10280b8e3a2cc9e3289e764d6bdc2a
OzgeD <OzgeD@users.noreply.github.com>
2021-08-19 01:07:21 +0300

    Make website from static to dynamic using MVC

--- ProductCatalog/Controllers/HomeController.cs
+++ ProductCatalog/Controllers/HomeController.cs
@@ -3,25 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ProductCatalog.Entity;
 
 namespace ProductCatalog.Controllers
 {
     public class HomeController : Controller
     {
+        DataContext _context = new DataContext();
+
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            return View(_context.Products.Where(i=>i.IsHome && i.IsApproved).ToList());
         }
 
-        public ActionResult Details()
+        public ActionResult Details(int id)
         {
-            return View();
+            return View(_context.Products.Where(i => i.Id == id).FirstOrDefault());
         }
 
         public ActionResult List()
         {
-            return View();
+            return View(_context.Products.Where(i => i.IsApproved).ToList());
         }
     }
 }
